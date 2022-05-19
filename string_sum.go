ff1829c133ec7ebea55032b27dd885d2b2520f4e
ZakirAvrora <krasavasula@gmail.com>
2022-05-19 22:46:55 +0600

    init

--- string_sum.go
+++ string_sum.go
@@ -2,9 +2,12 @@ package string_sum
 
 import (
 	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 )
 
-//use these errors as appropriate, wrapping them with fmt.Errorf function
+// use these errors as appropriate, wrapping them with fmt.Errorf function
 var (
 	// Use when the input is empty, and input is considered empty if the string contains only whitespace
 	errorEmptyInput = errors.New("input is empty")
@@ -23,5 +26,64 @@ var (
 // Use the errors defined above as described, again wrapping into fmt.Errorf
 
 func StringSum(input string) (output string, err error) {
-	return "", nil
+	runes := []rune(strings.ReplaceAll(input, " ", ""))
+
+	if len(runes) == 0 {
+		return "", errorEmptyInput
+	}
+
+	operands, ok := CheckTwoOperands(string(runes))
+
+	if !ok {
+		return "", errorNotTwoOperands
+	}
+
+	sum := 0
+	for _, r := range operands {
+		num, err := strconv.Atoi(r)
+		if err != nil {
+			return "", err
+		}
+		sum += num
+	}
+
+	return strconv.Itoa(sum), nil
+}
+
+func IsRuneSign(r rune) bool {
+	return r == '-' || r == '+'
+}
+
+func CheckTwoOperands(input string) ([]string, bool) {
+	runes := []rune(input)
+	start := 0
+	var twoOperands []string
+	str := ""
+
+	if IsRuneSign(runes[0]) {
+		start = 1
+		if runes[0] == '-' {
+			str = "-"
+		}
+	}
+	// fmt.Println(string(runes))
+	for i := start; i < len(runes); i++ {
+		if IsRuneSign(runes[i]) {
+			// fmt.Println(str)
+			twoOperands = append(twoOperands, str)
+			str = string(runes[i])
+
+			continue
+		} else {
+			str += string(runes[i])
+			// fmt.Println("dd", str)
+		}
+	}
+	twoOperands = append(twoOperands, str)
+	fmt.Println(twoOperands)
+	if len(twoOperands) != 2 {
+		return nil, false
+	}
+
+	return twoOperands, true
 }
